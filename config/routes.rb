e6a483127e1b5b47fccbfa19629a7182f373aacb
Kyle Naizer <kyle.naizer@gmail.com>
2015-11-05 01:19:12 +0000

    Added image form to places with a modal

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Nomster::Application.routes.draw do
    root 'places#index'
 
    resources :places do
-     resources :comments, :photos, :only => :create
+     resources :comments, :only => :create
+     resources :photos, :only => :create
    end
       
   # The priority is based upon order of creation: first created -> highest priority.
@@ -14,7 +15,7 @@ Nomster::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+ 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
