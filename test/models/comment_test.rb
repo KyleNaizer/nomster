043af7e405847491ceb7c4d8ad2ae92a9f1eac40
Kyle Naizer <kyle.naizer@gmail.com>
2015-11-14 19:06:24 +0000

    Added test for humanized rating of comments.

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "hum_test" do 
+    comment = Comment.new(:rating => '1_star').humanized_rating
+    expected = 'one star'
+    actual = comment
+    assert_equal expected, actual
+  end 
 end
